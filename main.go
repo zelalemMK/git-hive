cf741dbca7f71412823a75a49fd8f7e37cfc7678
Zelalem Mekonnen <zelliott33@gmail.com>
2023-08-16 13:55:21 -0500

    part 2 complete, until the last part

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os/exec"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -8,7 +10,26 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello")
+		return c.SendString("hello\n")
+	})
+
+	app.Post("/cli", func(c *fiber.Ctx) error {
+		var command struct {
+			Command string `json:"command"`
+		}
+
+		if err := c.BodyParser(&command); err != nil {
+			return err
+		}
+
+		cmd := exec.Command("sh", "-c", command.Command)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			return err
+		}
+
+		return c.SendString(string(output))
+
 	})
 
 	app.Listen(":3000")
